92ebff58427fc647aef463753d8595dd49f79321
ZackBz <zb01406@gmail.com>
2025-07-09 18:39:48 -0400

    adds additional tool prompt

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env bun
 import { existsSync } from "node:fs";
-import { scaffold } from "@/core/scaffold";
+import { scaffold } from "@/cli/scaffold";
 import { renderTitle } from "@/utils/renderTitle";
 import * as p from "@clack/prompts";
 import chalk from "chalk";
@@ -10,46 +10,44 @@ p.intro(chalk.bgBlue("create-hydrastack-app"));
 await renderTitle();
 
 const projectConfig = await p.group(
-	{
-		projectName: () =>
-			p.text({
-				message: "What is the name of your project?",
-				validate: (value) => {
-					if (!value) return "Project name cannot be empty";
-					if (existsSync(value)) return "Directory already exists";
-					return;
-				},
-			}),
-		projectType: () =>
-			p.select({
-				message: "What starter do you need?",
-				options: [
-					{ value: "router", label: "TanStack Router" },
-					{ value: "api", label: "API" },
-					{ value: "start", label: "TanStack Start" },
-				],
-			}),
-		useQuery: ({ results }) => {
-			if (results.projectType === "start" || results.projectType === "router") {
-				return p.confirm({
-					message: "Would you like to include TanStack Query?",
-					initialValue: true,
-				});
-			}
-		},
-	},
-	{
-		onCancel: ({ results }) => {
-			p.cancel("Operation cancelled.");
-			process.exit(0);
-		},
-	},
+  {
+    projectName: () =>
+      p.text({
+        message: "What is the name of your project?",
+        validate: (value) => {
+          if (!value) return "Project name cannot be empty";
+          if (existsSync(value)) return "Directory already exists";
+          return;
+        },
+      }),
+    projectType: () =>
+      p.select({
+        message: "What starter do you need?",
+        options: [
+          { value: "start", label: "TanStack Start" },
+          { value: "router", label: "TanStack Router" },
+          { value: "api", label: "API" },
+        ],
+      }),
+    tools: () =>
+      p.multiselect({
+        message: "Select additional tools. (Press space to select)",
+        options: [
+          { value: "tailwind", label: "Tailwind" },
+          { value: "shadcn", label: "shadcn/ui" },
+          { value: "biome", label: "Biome", hint: "recommended" },
+          { value: "query", label: "TanStack Query" },
+        ],
+        required: false,
+      }),
+  },
+
+  {
+    onCancel: ({ results }) => {
+      p.cancel("Operation cancelled.");
+      process.exit(0);
+    },
+  },
 );
 
-scaffold({
-	...projectConfig,
-	useQuery:
-		typeof projectConfig.useQuery === "boolean"
-			? projectConfig.useQuery
-			: undefined,
-});
+scaffold(projectConfig);
--- src/types/project.ts
+++ src/types/project.ts
@@ -1,7 +1,7 @@
 export type ProjectType = "router" | "api" | "start";
 
 export interface ProjectConfig {
-	projectName: string;
-	projectType: ProjectType;
-	useQuery?: boolean | null;
+  projectName: string;
+  projectType: ProjectType;
+  tools?: Array<string>;
 }
