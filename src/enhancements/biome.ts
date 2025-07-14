12dceeee62eace3442e0b6de15bd531d7770e301
ZackBz <zb01406@gmail.com>
2025-07-14 17:58:59 -0400

    get enhancement flow started

--- src/enhancements/biome.ts
+++ src/enhancements/biome.ts
@@ -1,5 +1,9 @@
 import type { ProjectConfig } from "@/types/project";
-
+import { $ } from "bun";
 export async function addBiome(config: ProjectConfig): Promise<void> {
-  console.log("adding biome.....");
+  console.log("adding Biome.....");
+  await $`bun add -D -E @biomejs/biome`.cwd(config.projectName).quiet();
+  console.log("Installed Biome!");
+  console.log("Creating config file...");
+  await $`bunx --bun biome init`.quiet();
 }
