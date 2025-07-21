1560bba68f508a3a30f89839df98fac70a8c93d5
ZackBz <zb01406@gmail.com>
2025-07-21 16:31:23 -0400

    use path to mitigate cross platform issues

--- src/enhancements/git-ignore.ts
+++ src/enhancements/git-ignore.ts
@@ -1,6 +1,8 @@
 import type { ProjectConfig } from "@/types/project";
+import { join } from "node:path";
 
 export const addGitIgnore = async (config: ProjectConfig): Promise<void> => {
-	const configPath = `${config.projectName}`;
-	console.log(configPath, "from git ignore");
+  const gitIgnorePath = join(config.projectName, ".gitignore");
+
+  await Bun.write(gitIgnorePath, gitIgnoreContent);
 };
