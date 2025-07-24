68fc91beba54c748dcf0c7dda71a5c7070738a84
ZackBz <zb01406@gmail.com>
2025-07-24 14:35:38 -0400

    fix gitignore with start-bare

--- src/enhancements/git-ignore.ts
+++ src/enhancements/git-ignore.ts
@@ -1,14 +1,14 @@
 import type { ProjectConfig } from "@/types/project";
 import { join } from "node:path";
+import { $ } from "bun";
 
 export const addGitIgnore = async (config: ProjectConfig): Promise<void> => {
   const gitIgnorePath = join(config.projectName, ".gitignore");
-
-  const addedIgnores = `
+  const ignores = `
 .idea
 .vscode
 `;
+  await $`touch ${gitIgnorePath}`;
 
-  const gitIgnoreContent = await Bun.file(gitIgnorePath).text();
-  await Bun.write(gitIgnorePath, gitIgnoreContent + addedIgnores);
+  await Bun.write(gitIgnorePath, ignores);
 };
